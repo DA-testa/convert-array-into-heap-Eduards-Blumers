0516778c55ec77861767867d122a5de993e34fab
221RDB194 <126565258+Eduards-Blumers@users.noreply.github.com>
2023-03-20 01:42:19 +0000

    test4

--- build_heap.py
+++ build_heap.py
@@ -1,9 +1,8 @@
 # python3
 
 def heapify(data, n, i, swaps):
-
-    smallest = i  
-    l = 2 * i + 1 
+    smallest = i
+    l = 2 * i + 1
     r = 2 * i + 2
 
     if l < n and data[l] < data[smallest]:
@@ -21,21 +20,35 @@ def heapify(data, n, i, swaps):
 def build_heap(data):
     swaps = []
     n = len(data)
+
     for i in range(n // 2 - 1, -1, -1):
         heapify(data, n, i, swaps)
     return swaps
 
 
 def main():
-    
-    n = int(input().strip())
-    data = list(map(int, input().split()))
+
+    input_type = input("I or F: ")
+    if "I" in input_type:
+        n = int(input())
+        data = list(map(int, input().split()))
+
+
+    elif "F" in input_type:
+        filename = input("File name: ")
+        with open("tests/"+filename, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+
     assert len(data) == n
     swaps = build_heap(data)
+    print("")
     print(len(swaps))
+
     for i, j in swaps:
         print(i, j)
 
 
 if __name__ == "__main__":
     main()
+
